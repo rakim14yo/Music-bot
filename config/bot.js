2b17c6ea30a506f29253abddadc1ce3dbb2dbd6c
rakim14yo <85509829+rakim14yo@users.noreply.github.com>
2021-07-06 13:11:00 +0600

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODU1ODc4MjMyOTcyMTk3OTY5.YM44uA.-YA7ltyw1oiDZSkPWZvRq4xD91o',
+        prefix: 'S.',
+        activity: 'S.help and S.play',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
